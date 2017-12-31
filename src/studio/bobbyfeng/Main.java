c8963303016723334e28653454768a3c1a729ef2
Feng Bo <bobbyfeng@cse.com.sg>
2017-12-31 18:22:45 +0800

    added part 1 of Lambda Challenge exercise

--- src/studio/bobbyfeng/Main.java
+++ src/studio/bobbyfeng/Main.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -61,37 +63,84 @@ public class Main {
 
 		departments.stream().flatMap(department -> department.getEmployees().stream()).forEach(System.out::println);
 
-//		List<String> sortedGNumbers = someBingoNumbers
-//				.stream()
-//				.map(String::toUpperCase)
-//				.filter(s -> s.startsWith("G"))
-//				.sorted()
-//				.collect(Collectors.toList());
-		List<String> sortedGNumbers = someBingoNumbers
-				.stream()
-				.map(String::toUpperCase)
-				.filter(s -> s.startsWith("G"))
-				.sorted()
-				.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-		
-		for(String s : sortedGNumbers) {
+		// List<String> sortedGNumbers = someBingoNumbers
+		// .stream()
+		// .map(String::toUpperCase)
+		// .filter(s -> s.startsWith("G"))
+		// .sorted()
+		// .collect(Collectors.toList());
+		List<String> sortedGNumbers = someBingoNumbers.stream().map(String::toUpperCase).filter(s -> s.startsWith("G"))
+				.sorted().collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
+		for (String s : sortedGNumbers) {
 			System.out.println(s);
 		}
-		
+
 		Map<Integer, List<Employee>> groupedByAge = departments.stream()
 				.flatMap(department -> department.getEmployees().stream())
 				.collect(Collectors.groupingBy(employee -> employee.getAge()));
-		
-		departments.stream()
-			.flatMap(department -> department.getEmployees().stream())
-			.reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1:e2)
-			.ifPresent(System.out::println);
-		
-		Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
-			.filter(s -> {
-				System.out.println(s);
-				return s.length() ==3;
-			}).count();
+
+		departments.stream().flatMap(department -> department.getEmployees().stream())
+				.reduce((e1, e2) -> e1.getAge() < e2.getAge() ? e1 : e2).ifPresent(System.out::println);
+
+		Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST").filter(s -> {
+			System.out.println(s);
+			return s.length() == 3;
+		}).count();
+
+		// Lambda Challenge No. 1
+		Runnable runnable = () -> {
+			String myString = "Let's split this up into an array";
+
+			String[] parts = myString.split(" ");
+			for (String part : parts) {
+				System.out.println(part);
+			}
+		};
+
+		// Lambda Challenge No. 2
+		Function<String, String> lambdaFunction = source -> {
+			StringBuilder returnVal = new StringBuilder();
+			for (int i = 0; i < source.length(); i++) {
+				if (i % 2 == 1) {
+					returnVal.append(source.charAt(i));
+				}
+			}
+
+			return returnVal.toString();
+		};
+
+		// Lambda Challenge No. 3
+		System.out.println(lambdaFunction.apply("0123456789"));
+
+		// Lambda Challenge No. 5
+		System.out.println(everySecondCharacter(lambdaFunction, "1234567890"));
+
+		// Lambda Challenge No. 6
+		Supplier<String> iLoveJava = () -> {
+			return "I Love Java";
+		};
+
+		// Lambda Challenge No. 7
+		String supplierResult = iLoveJava.get();
+		System.out.println(supplierResult);
+	}
+
+	// Lambda Challenge No. 4
+	public final static String everySecondCharacter(Function<String, String> lambdaFunction, String s1) {
+
+		return lambdaFunction.apply(s1);
+	}
+
+	public static String everySecondChar(String source) {
+		StringBuilder returnVal = new StringBuilder();
+		for (int i = 0; i < source.length(); i++) {
+			if (i % 2 == 1) {
+				returnVal.append(source.charAt(i));
+			}
+		}
+
+		return returnVal.toString();
 	}
 
 }
